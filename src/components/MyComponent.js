3344472c2365b078595a9a35df316e92d75d6c68
TruongNgan6 <truongvokimngan13@gmail.com>
2023-08-03 10:33:34 +0700

    video 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,53 +1,13 @@
 // class component
 // function component
 import React from "react";
+import UserInfo from "./UserInfo";
 class MyComponent extends React.Component {
-  state = {
-    name: "Nancy",
-    address: "Ho Chi Minh",
-    age: 20,
-  };
-  // handleOnclick = (event) => {
-  //   console.log(">> Check click me!!");
-  //   this.setState({
-  //     name: "Eric",
-  //     age: Math.floor(Math.random() * 100 + 1),
-  //   });
-  // };
-
-  // handleOnMouseOver(event) {
-  //   console.log(event.pageX);
-  // }
-
-  handleOnChange = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-    // console.log(event, event.target.value);
-  };
-
-  handleOnSubmit = (event) => {
-    event.preventDefault();
-    console.log("<<<check handle on submit", this.state);
-  };
   // JSX
   render() {
     return (
       <div>
-        My name is {this.state.name} and I'm {this.state.age}
-        <form
-          onSubmit={(event) => {
-            this.handleOnSubmit(event);
-          }}
-        >
-          <input
-            type="text"
-            onChange={(event) => {
-              this.handleOnChange(event);
-            }}
-          />
-          <button>Submit</button>
-        </form>
+        <UserInfo></UserInfo>
       </div>
     );
   }
---
+++ src/components/UserInfo.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+class UserInfo extends React.Component {
+  state = {
+    name: "Nancy",
+    address: "Ho Chi Minh",
+    age: 20,
+  };
+
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleOnChangeAge = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  handleOnChangeAdress = (event) => {
+    this.setState({
+      address: event.target.value,
+    });
+  };
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log("<<<check handle on submit", this.state);
+  };
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm {this.state.age} and I'm from{" "}
+        {this.state.address}
+        <form
+          onSubmit={(event) => {
+            this.handleOnSubmit(event);
+          }}
+        >
+          <label>Your name </label>
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => {
+              this.handleOnChange(event);
+            }}
+          />
+          <label> Your age </label>
+          <input
+            value={this.state.age}
+            type="text"
+            onChange={(event) => {
+              this.handleOnChangeAge(event);
+            }}
+          />
+
+          <label> Your Address</label>
+          <input
+            value={this.state.address}
+            type="text"
+            onChange={(event) => {
+              this.handleOnChangeAdress(event);
+            }}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
