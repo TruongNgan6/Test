e2ae165308d3e39b45c6ee283d4da45e16acfcc6
TruongNgan6 <truongvokimngan13@gmail.com>
2023-08-04 22:52:06 +0700

    video 25

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class DisplayInfo extends React.Component {
+  state = {
+    isShowHideList: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowHideList: !this.state.isShowHideList,
+    });
+  };
   render() {
     // console.log(this.props);
     const { listUsers } = this.props;
@@ -8,18 +16,31 @@ class DisplayInfo extends React.Component {
     //Props
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>My name is {user.name}</div>
-              <div>I'm {user.age}</div>
-              <div>And I'm from {user.address}</div>
-              <hr />
-            </div>
-          );
-        })}
-        {/* <div>My name is {name}</div>
-        <div>I'm {age}</div> */}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowHideList === true
+              ? "Hide list users"
+              : "Show list users"}
+          </span>
+        </div>
+        {this.state.isShowHideList && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age < 30 ? "green" : "red"}>
+                  <div>My name is {user.name}</div>
+                  <div>I'm {user.age}</div>
+                  <div>And I'm from {user.address}</div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,9 +6,9 @@ import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
   state = {
     listUsers: [
-      { id: 1, name: "Nancy", age: 20, address: "Ho Chi Minh" },
-      { id: 2, name: "John", age: 30, address: "Seoul" },
-      { id: 3, name: "Shin", age: 27, address: "Ha Noi" },
+      { id: 1, name: "Nancy", age: "20", address: "Ho Chi Minh" },
+      { id: 2, name: "John", age: "32", address: "Seoul" },
+      { id: 3, name: "Shin", age: "27", address: "Ha Noi" },
     ],
   };
   // JSX
